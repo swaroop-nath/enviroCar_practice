0436a90ded2fb532b316229e63f0a65b05d6a078
Swaroop Nath <swaroopnath6@gmail.com>
2019-04-05 22:58:04 +0530

    Bin Structure Updated
    Latest Bin Structure accommodates the facility of incorporating average value of the phenomenon for a location throughout the day.

--- bin_structure.py
+++ bin_structure.py
@@ -3,9 +3,20 @@ class BIN:
         self.latlng = (x_left, y_bottom)
         self.length = bin_size
         self.feature_dict = {}
+        self.feature_size = {}
     def set_feature(self, feature, date):
-        self.feature_dict[date] = feature
+        if self.feature_dict.get(date) == None:
+            self.feature_dict[date] = feature
+            self.feature_size[date] = 1
+        else:
+            self.feature_dict[date] = self.feature_dict.get(date) + feature
+            self.feature_size[date] = self.feature_size.get(date) + 1
+            
+    def run_average_feature_setter(self):
+        for date,value in self.feature_dict.items():
+            self.feature_dict[date] = value/self.feature_size[date]
+            
     def get_feature(self, date = 0):
         if date != 0:
-            return self.feature_dict[date]
+            return self.feature_dict.get(date)
         return self.feature_dict
